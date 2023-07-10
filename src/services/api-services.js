a2741807c13b5f28e0b5495256a47f6ab406a2ad
Jaison J <jaistarx.in@gmail.com>
2023-07-10 16:07:28 +0530

    added proxy to handle cors

--- src/services/api-services.js
+++ src/services/api-services.js
@@ -2,6 +2,7 @@ import axios from "axios";
 import cheerio from "cheerio";
 
 const BASE_URL = "https://webpage-scraper-backend-django.vercel.app/tableview/"
+const CORS_PROXY_URL = 'https://cors-anywhere.herokuapp.com/';
 
 export async function fetchTableData() {
   const res = await axios.get(BASE_URL);
@@ -37,28 +38,30 @@ export async function deleteRowInTable(id) {
 
 export async function getWebsiteInfo(url) {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get(CORS_PROXY_URL + url);
     const html = response.data;
 
     const $ = cheerio.load(html);
     const text = $("body").text();
     const wordCount = text.trim().split(/\s+/).length;
 
-    const webLinks = [];
-    const mediaLinks = [];
+    const webLinks = new Set();
+    const mediaLinks = new Set();
 
     $("a").each((index, element) => {
-      webLinks.push($(element).attr("href"));
+      const link = $(element).attr("href");
+      webLinks.add(link);
     });
 
     $("img, video").each((index, element) => {
-      mediaLinks.push($(element).attr("src"));
+      const link = $(element).attr("src");
+      mediaLinks.add(link);
     });
 
     return {
       word_count: wordCount,
-      web_links: webLinks,
-      media_links: mediaLinks,
+      web_links: Array.from(webLinks),
+      media_links: Array.from(mediaLinks),
     };
   } catch (error) {
     console.error("Error scraping web:", error);
